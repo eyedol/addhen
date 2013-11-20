88c37523e0cfb322c976c815ce14fc979bfbac59
Henry Addo <addhen@linuxmail.org>
2013-11-21 01:55:41 +0900

    Initial page setup

--- javascripts/flickr.js
+++ javascripts/flickr.js
@@ -22,84 +22,26 @@ $(function () {
                 '.flickr.com/services/rest/',
         data: {
             format: 'json',
-            method: 'flickr.interestingness.getList',
-            api_key: '7617adae70159d09ba78cfec73c13be3'
+            method: 'flickr.people.getPublicPhotos',
+            user_id: '73308752@N00',
+            per_page: 35,
+            api_key: '7ffd3c4b9d9f3a486b67124d5b530f11'
         },
         dataType: 'jsonp',
         jsonp: 'jsoncallback'
     }).done(function (result) {
-        var linksContainer = $('#links'),
-            baseUrl;
+        $('#pics').append('<ul id="list"></ul>');
+        var baseUrl;
         // Add the demo images as links with thumbnails to the page:
         $.each(result.photos.photo, function (index, photo) {
             baseUrl = 'http://farm' + photo.farm + '.static.flickr.com/' +
                 photo.server + '/' + photo.id + '_' + photo.secret;
-            $('<a/>')
-                .append($('<img>').prop('src', baseUrl + '_s.jpg'))
-                .prop('href', baseUrl + '_b.jpg')
-                .prop('title', photo.title)
-                .attr('data-gallery', '')
-                .appendTo(linksContainer);
+            $('<a/>').append(
+                    $('<img>').prop('src', baseUrl + '_q.jpg')
+            ).prop('href', baseUrl + '_b.jpg')
+             .prop('title', photo.title)
+             .attr('data-gallery', '')
+             .appendTo($('<li></li>').appendTo('#list'));
         });
     });
-
-    $('#borderless-checkbox').on('change', function () {
-        var borderless = $(this).is(':checked');
-        $('#blueimp-gallery').data('useBootstrapModal', !borderless);
-        $('#blueimp-gallery').toggleClass('blueimp-gallery-controls', borderless);
-    });
-
-    $('#fullscreen-checkbox').on('change', function () {
-        $('#blueimp-gallery').data('fullScreen', $(this).is(':checked'));
-    });
-
-    $('#image-gallery-button').on('click', function (event) {
-        event.preventDefault();
-        blueimp.Gallery($('#links a'), $('#blueimp-gallery').data());
-    });
-
-    $('#video-gallery-button').on('click', function (event) {
-        event.preventDefault();
-        blueimp.Gallery([
-            {
-                title: 'Sintel',
-                href: 'http://media.w3.org/2010/05/sintel/trailer.mp4',
-                type: 'video/mp4',
-                poster: 'http://media.w3.org/2010/05/sintel/poster.png'
-            },
-            {
-                title: 'Big Buck Bunny',
-                href: 'http://upload.wikimedia.org/wikipedia/commons/7/75/' +
-                    'Big_Buck_Bunny_Trailer_400p.ogg',
-                type: 'video/ogg',
-                poster: 'http://upload.wikimedia.org/wikipedia/commons/thumb/7/70/' +
-                    'Big.Buck.Bunny.-.Opening.Screen.png/' +
-                    '800px-Big.Buck.Bunny.-.Opening.Screen.png'
-            },
-            {
-                title: 'Elephants Dream',
-                href: 'http://upload.wikimedia.org/wikipedia/commons/transcoded/8/83/' +
-                    'Elephants_Dream_%28high_quality%29.ogv/' +
-                    'Elephants_Dream_%28high_quality%29.ogv.360p.webm',
-                type: 'video/webm',
-                poster: 'http://upload.wikimedia.org/wikipedia/commons/thumb/9/90/' +
-                    'Elephants_Dream_s1_proog.jpg/800px-Elephants_Dream_s1_proog.jpg'
-            },
-            {
-                title: 'LES TWINS - An Industry Ahead',
-                href: 'http://www.youtube.com/watch?v=zi4CIXpx7Bg',
-                type: 'text/html',
-                youtube: 'zi4CIXpx7Bg',
-                poster: 'http://img.youtube.com/vi/zi4CIXpx7Bg/0.jpg'
-            },
-            {
-                title: 'KN1GHT - Last Moon',
-                href: 'http://vimeo.com/73686146',
-                type: 'text/html',
-                vimeo: '73686146',
-                poster: 'http://b.vimeocdn.com/ts/448/835/448835699_960.jpg'
-            }
-        ], $('#blueimp-gallery').data());
-    });
-
 });
